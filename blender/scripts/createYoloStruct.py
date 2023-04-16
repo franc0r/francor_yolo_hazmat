d2f80b5e04a6a35227e4f0818f5292dfb2b595c2
Martin Bauernschmitt <martin.bauernschmitt@francor.de>
2023-04-16 18:37:10 +0200

    Initial commit

---
+++ blender/scripts/createYoloStruct.py
@@ -0,0 +1,82 @@
+#/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
+
+blenderDataPath     = "G:\\\\YoloHazmat\\Hazmat_TrainData\\out"
+yoloDataPath        = "G:\\\\YoloHazmat\\Hazmat_TrainData\\yoloDataset"
+
+trainDataPercentage = 0.8
+validDataPercentage = 0.19
+testDataPercentage  = 0.01
+
+# Remove directory if exists
+if os.path.exists(yoloDataPath):
+    print("Removing directory: " + yoloDataPath)
+
+    # Remove even if it contains data
+    shutil.rmtree(yoloDataPath)
+
+# Create directory structure
+print("Creating directory structure for YOLO")
+
+os.mkdir(yoloDataPath)
+os.mkdir(yoloDataPath + "\\hazmat")
+os.mkdir(yoloDataPath + "\\hazmat\\images")
+os.mkdir(yoloDataPath + "\\hazmat\\images\\train")
+os.mkdir(yoloDataPath + "\\hazmat\\images\\valid")
+os.mkdir(yoloDataPath + "\\hazmat\\images\\test")
+os.mkdir(yoloDataPath + "\\hazmat\\labels")
+os.mkdir(yoloDataPath + "\\hazmat\\labels\\train")
+os.mkdir(yoloDataPath + "\\hazmat\\labels\\valid")
+os.mkdir(yoloDataPath + "\\hazmat\\labels\\test")
+
+# Copy images
+print("Copying images")
+
+# Get all files in directory
+files = os.listdir(blenderDataPath)
+
+imgLst = []
+descLst = []
+
+# Get all .jpg files and store them in a list
+for file in files:
+    if file.endswith(".jpg"):
+        imgLst.append(file)
+
+# Calculate number of images for train, valid and test set
+numImgs = len(imgLst)
+numTrainImgs = int(float(numImgs) * trainDataPercentage)
+numValidImgs = int(float(numImgs) * validDataPercentage)
+numTestImgs = int(float(numImgs) * testDataPercentage)
+
+# Loop through images
+for idx, file in enumerate(imgLst):
+    descName = file.split(".")[0] + ".txt"
+
+    # Check if file is in train or valid set
+    if idx < numTrainImgs:
+        shutil.copy2(blenderDataPath + "\\" + file, yoloDataPath + "\\hazmat\\images\\train\\" + file)
+        shutil.copy2(blenderDataPath + "\\" + descName, yoloDataPath + "\\hazmat\\labels\\train\\" + descName)
+    else:
+        if idx < (numTrainImgs + numValidImgs):
+            # Copy file to valid set
+            shutil.copy2(blenderDataPath + "\\" + file, yoloDataPath + "\\hazmat\\images\\valid\\" + file)
+            shutil.copy2(blenderDataPath + "\\" + descName, yoloDataPath + "\\hazmat\\labels\\valid\\" + descName)
+        else:
+            # Copy file to test set
+            shutil.copy2(blenderDataPath + "\\" + file, yoloDataPath + "\\hazmat\\images\\test\\" + file)
+            shutil.copy2(blenderDataPath + "\\" + descName, yoloDataPath + "\\hazmat\\labels\\test\\" + descName)
+            
+
+# Get number of images in train and valid set
+numTrainImgs = len(os.listdir(yoloDataPath + "\\hazmat\\images\\train"))
+numValidImgs = len(os.listdir(yoloDataPath + "\\hazmat\\images\\valid"))
+numTestImgs = len(os.listdir(yoloDataPath + "\\hazmat\\images\\test"))
+
+# Print number of images
+print("Number of images in train set: " + str(numTrainImgs))
+print("Number of images in valid set: " + str(numValidImgs))
+print("Number of images in test set: " + str(numTestImgs))
---
+++ blender/scripts/showBoundingBox.py
@@ -0,0 +1,91 @@
+#/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Open an image and draw a bounding box around the object
+# Path: showBoundingBox.py
+
+import cv2 as cv
+
+imgID    = 0
+filePath = '../out/'
+
+hazmatList = [
+    ("NON-FLAMMABLE GAS", "2.png"),
+    ("FLAMMABLE_LIQUID", "4.png"),
+    ("FLAMMABLE_SOLID_1", "5.png"),
+    ("OXIDIZER", "6.png"),
+    ("RADIOACTIVE_II", "7.png"),
+    ("CORROSIVE", "8.png"),
+    ("INHALATION_HAZARD_1", "9.png"),
+    ("INFECTIOUS_SUBSTANCE", "10.png"),
+    ("EXPLOSIVE_1", "11.png"),
+    ("COMBUSTIBLE_1", "12.png"),
+    ("DANGEROUS_WHEN_WET_1", "13.png"),
+    ("ORGANIC_PEROXIDE", "14.png"),
+    ("INHALATION_HAZARD_6", "35.png"),
+    ("TOXIC", "36.png"),
+    ("COMBUSTIBLE_2", "47.png"),
+    ("FLAMMABLE", "48.png"),
+    ("GASOLINE", "50.png"),
+    ("FISSILE", "58.png"),
+    ("RADIOACTIVE_I", "59.png"),
+    ("EXPLOSIVE_2", "97.png"),
+    ("EXPLOSIVE_3", "98.png"),
+    ("FLAMMABLE_SOLID_2", "100.png"),
+    ("DANGEROUS_WHEN_WET_2", "101.png"),
+    ("SPONTANEOUSLY_COMBUSTIBLE", "102.png"),
+    ("DANGEROUS_WHEN_WET_3", "103.png"),
+]
+
+# Filename
+filename = filePath + str(imgID)
+
+# Open image
+img = cv.imread(filename + '.jpg')
+
+# Get image resolution
+imgHeight, imgWidth, channels = img.shape
+
+# Print image width and height
+print("Image width: %d Img height: %d" % (imgWidth, imgHeight))
+
+# Open descriptor txt file
+with open(filename + '.txt', 'r') as f:
+    # Read all lines
+    lines = f.readlines()
+
+    # Color list
+    colors = [(0, 0, 255), (0, 255, 0), (255, 0, 0), (255, 255, 0)]
+
+    # Loop through lines
+    for idx, line in enumerate(lines):
+        # Split line
+        line = line.split(' ')
+
+        # Get hazmat class
+        hazmatClass = line[0]
+        hazmatName = hazmatList[int(hazmatClass)][0]
+
+        xCenter = float(line[1]) * imgWidth
+        yCenter = float(line[2]) * imgHeight
+        width = float(line[3]) * imgWidth
+        height = float(line[4]) * imgHeight
+
+        # Print bounding box
+        print("Bounding box: %s %f %f %f %f" % (hazmatName, xCenter, yCenter, width, height))
+
+        # Calculate start and end point
+        startPoint = (int(xCenter - (width / 2)), int(yCenter - (height / 2)))
+        endPoint = (int(xCenter + (width / 2)), int(yCenter + (height / 2)))
+        txtPos = (int(xCenter - (width / 2)), int(yCenter - (height / 2) + 10))
+
+        # Draw bounding box
+        cv.rectangle(img, startPoint, endPoint, colors[idx], 2)
+
+        # Draw text
+        cv.putText(img, hazmatName, txtPos, cv.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
+
+# Show image
+cv.imshow('image', img)
+
+cv.waitKey(0)
