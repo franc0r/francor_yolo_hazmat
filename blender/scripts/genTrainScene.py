299248778838a61f51fc3506b35fc6bd14385d88
Martin Bauernschmitt <martin.bauernschmitt@francor.de>
2023-04-19 22:27:37 +0200

    Set background color to black

--- blender/scripts/genTrainScene.py
+++ blender/scripts/genTrainScene.py
@@ -16,7 +16,8 @@ import os
 class BlenderHandler:
     '''Blender handler class for blender functions'''
     def __init__(self):
-        pass
+        # Set scene background to black
+        bpy.context.scene.world.node_tree.nodes["Background"].inputs[0].default_value = (0.0, 0.0, 0.0, 1.0)
 
     def add_background_plane(self, name, size, location, rotation):
         '''Add plane'''
@@ -62,6 +63,24 @@ class BlenderHandler:
             object.data.materials[0] = material
         else:
             object.data.materials.append(material)
+
+    def add_light(self, name, location, rotation):
+        '''Add light'''
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.ops.object.light_add(type='POINT', location=location, rotation=rotation)
+        object = bpy.context.active_object
+        object.name = name
+
+        return object
+
+    def add_camera(self, name, location, rotation):
+        '''Add camera'''
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.ops.object.camera_add(location=location, rotation=rotation)
+        object = bpy.context.active_object
+        object.name = name
+
+        return object
 
     def delete_all_elements(self):
         '''Delete all elements in the scene'''
@@ -123,6 +142,31 @@ class TrainScene:
         object = self._blender.add_background_plane(name, size, location, rotation)
         self._blender.add_material(object, self._img_path + texture_filename)
 
+    def add_light(self):
+        '''Add light'''
+        name = 'Light'
+        location = (0.0, -2.0, 1.0)
+        rotation = (0.0, 0.0, 0.0)
+
+        # Add light as sun 
+        object = self._blender.add_light(name, location, rotation)
+        object.data.energy = 250.0
+        object.data.shadow_soft_size = 0.1
+
+    def add_camera(self):
+        '''Add camera'''
+        name = 'Camera'
+        location = (0.0, -6.0, 0.0)
+        rotation = (math.radians(90), 0.0, 0.0)
+
+        object = self._blender.add_camera(name, location, rotation)
+        object.data.lens = 35.0
+        object.data.sensor_width = 32.0
+        object.data.sensor_height = 18.0
+        object.data.clip_start = 0.1
+        object.data.clip_end = 100.0
+
+
     def cleanup_scene(self):
         '''Cleanup scene'''
         self._blender.delete_all_elements()
@@ -138,7 +182,10 @@ if __name__ == "__main__":
     blender = BlenderHandler()
     train_scene = TrainScene(blender)
     train_scene.cleanup_scene()
+
     train_scene.add_background('BackgroundPlane', 'wood_texture_01.jpg')
+    train_scene.add_light()
+    train_scene.add_camera()
     
 
     # Print blender object names
