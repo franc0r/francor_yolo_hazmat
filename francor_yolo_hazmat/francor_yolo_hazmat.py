942ffc6bd057837ad7ee71d603222b2d3a0fa070
Martin Bauernschmitt <martin.bauernschmitt@francor.de>
2023-04-24 19:45:55 +0200

    Added service to enable hazmat

--- francor_yolo_hazmat/francor_yolo_hazmat.py
+++ francor_yolo_hazmat/francor_yolo_hazmat.py
@@ -8,6 +8,7 @@ from rclpy.node import Node
 from cv_bridge import CvBridge
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
+from std_srvs.srv import SetBool
 from threading import Lock
 import cv2
 
@@ -19,6 +20,9 @@ class ImageSubscriber(Node):
         self.__img_subs = self.create_subscription(Image, '/image_raw', self.listener_callback, 1)
         self.__img_pub = self.create_publisher(Image, '/yolov5/image', 10)
 
+        # Create bool service callback
+        self.__bool_srv = self.create_service(SetBool, '/hazmat_yolo/enable', self.bool_srv_callback)
+
         # Create 10 hz timer
         self.__timer = self.create_timer(0.2, self.timer_callback)
         
@@ -29,51 +33,67 @@ class ImageSubscriber(Node):
         # Image container
         self.__cv_image = None
         self.__cv_image_lock = Lock()
+        self.__detection_enabled = False
 
         # Last detections
         self.__last_detection_lst = None
 
         self.get_logger().info("Node Initialized")
 	
+    def bool_srv_callback(self, request, response):
+        self.__detection_enabled = request.data
+        response.success = True
+        response.message = "Service call successful"
+        return response
+
     def timer_callback(self):
         # Process image with model
+        detection_lst = []
+
         with self.__cv_image_lock:
             if self.__cv_image is None:
                 return
-
-            # Run model
-            results = self.__model(self.__cv_image)
-
-            # Detection list
-            detection_lst = []
             
-            # Loop through all detection as tupples
-            for detection in results.pandas().xyxy[0].itertuples():
-                confidence = detection.confidence
+            # Only if detection is enabled
+            if self.__detection_enabled:
 
-                if confidence < 0.85:
-                    continue
-                
-                class_name = detection.name
-                p1 = (int(detection.xmin), int(detection.ymin))
-                p2 = (int(detection.xmax), int(detection.ymax))
+                # Run model
+                results = self.__model(self.__cv_image)
 
-                # Calculate size
-                size = (p2[0] - p1[0]) * (p2[1] - p1[1])
+                # Loop through all detection as tupples
+                for detection in results.pandas().xyxy[0].itertuples():
+                    confidence = detection.confidence
 
-                # Calculate image size
-                image_size = self.__cv_image.shape[0] * self.__cv_image.shape[1]
+                    if confidence < 0.85:
+                        continue
+                    
+                    class_name = detection.name
+                    p1 = (int(detection.xmin), int(detection.ymin))
+                    p2 = (int(detection.xmax), int(detection.ymax))
 
-                # Only if size is smaller than 40% of image size continue
-                if size > image_size * 0.3:
-                    continue
+                    # Calculate size
+                    size = (p2[0] - p1[0]) * (p2[1] - p1[1])
 
-                # Draw rectangle
-                cv2.rectangle(self.__cv_image, p1, p2, (0, 255, 0), 2)
+                    # Calculate image size
+                    image_size = self.__cv_image.shape[0] * self.__cv_image.shape[1]
 
-                # Draw text
-                cv2.putText(self.__cv_image, class_name, p1, cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 0), 1)
+                    # Only if size is smaller than 40% of image size continue
+                    if size > image_size * 0.3:
+                        continue
+                    
+                    # Append to detection list
+                    detection_lst.append((class_name, p1, p2))
 
+                    # Draw rectangle
+                    cv2.rectangle(self.__cv_image, p1, p2, (0, 0, 255), 2)
+
+                    # Draw text
+                    cv2.putText(self.__cv_image, class_name, (p1[0], p1[1] + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 0, 0), 1)
+
+            # Loop through detection list
+            for detection in detection_lst:
+                # Draw fonts on left corner of image
+                cv2.putText(self.__cv_image, detection[0], (10, 10 + 10 * detection_lst.index(detection)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
             # Publish image
             self.__img_pub.publish(self.__cv_br.cv2_to_imgmsg(self.__cv_image, encoding="rgb8"))
