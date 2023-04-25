a23fb0d34c1143e5c31e8042c53f09a0176f7b05
Martin Bauernschmitt <martin.bauernschmitt@francor.de>
2023-04-25 18:55:39 +0200

    Updated hazmat detection

--- francor_yolo_hazmat/francor_yolo_hazmat.py
+++ francor_yolo_hazmat/francor_yolo_hazmat.py
@@ -22,21 +22,21 @@ class ImageSubscriber(Node):
         super().__init__('yolov5_node')
 
         # Ros2 get package path
-        self.__package_path = os.path.abspath("")
-        self.__yolo_path = os.path.abspath("yolov5")
-        self.__model_path = os.path.abspath("yolov5/hazmat_yolov5s_200e.pt")
+        self.__package_path = '/home/martin/Projekte/francor/ros2_ws/src/francor_yolo_hazmat/'
+        self.__yolo_path = self.__package_path + 'yolov5'
+        self.__model_path = self.__package_path + 'yolov5/2023-04-25-02-yolo5s-128e.pt'
 
         self.get_logger().info("Package path: " + self.__package_path)
 
         # declare pubsub routes
-        self.__img_subs = self.create_subscription(Image, '/camera/tele/image_raw', self.listener_callback, qos_profile=qos_profile_sensor_data)
+        self.__img_subs = self.create_subscription(Image, '/image_raw', self.listener_callback, qos_profile=qos_profile_sensor_data)
         self.__img_pub = self.create_publisher(CompressedImage, '/yolov5/image/compressed', qos_profile=qos_profile_sensor_data)
 
         # Create bool service callback
         self.__bool_srv = self.create_service(SetBool, '/hazmat_yolo/enable', self.bool_srv_callback)
 
         # Create 10 hz timer
-        self.__timer = self.create_timer(0.2, self.timer_callback)
+        self.__timer = self.create_timer(0.1, self.timer_callback)
         
         # declare tools
         self.__cv_br = CvBridge()
@@ -66,15 +66,10 @@ class ImageSubscriber(Node):
         with self.__cv_image_lock:
             if self.__cv_image is None:
                 return
+                
             
             # Only if detection is enabled
-            if self.__detection_enabled:
-
-                # Convert to greyscale
-                #self.__cv_image = cv2.cvtColor(self.__cv_image, cv2.COLOR_BGR2RGB)
-                self.__cv_image = cv2.cvtColor(self.__cv_image, cv2.COLOR_BGR2GRAY)
-                self.__cv_image = cv2.cvtColor(self.__cv_image, cv2.COLOR_GRAY2RGB)
-
+            if self.__detection_enabled:              
                 # Run model
                 results = self.__model(self.__cv_image)
 
@@ -82,7 +77,7 @@ class ImageSubscriber(Node):
                 for detection in results.pandas().xyxy[0].itertuples():
                     confidence = detection.confidence
 
-                    if confidence < 0.6:
+                    if confidence < 0.5:
                         continue
                     
                     class_name = detection.name
@@ -90,15 +85,24 @@ class ImageSubscriber(Node):
                     p2 = (int(detection.xmax), int(detection.ymax))
 
                     # Calculate size
-                    size = (p2[0] - p1[0]) * (p2[1] - p1[1])
+                    x_size = p2[0] - p1[0]
+                    y_size = p2[1] - p1[1]
+                    size = x_size * y_size
+
+                    # Calculate ratio
+                    ratio = x_size / y_size
+
 
                     # Calculate image size
                     image_size = self.__cv_image.shape[0] * self.__cv_image.shape[1]
 
-                    # Only if size is smaller than 40% of image size continue
-                    if size > image_size * 0.3:
+                    # Only if size is smaller than 50% of image size continue
+                    if size > image_size * 0.5:
                         continue
-                    
+
+                    #if ratio > 1.7 or ratio < 0.3:
+                    #    self.get_logger().error("Invalid ratio: %.3f" % ratio)
+                    #    continue
                     # Append to detection list
                     detection_lst.append((class_name, p1, p2))
 
@@ -106,16 +110,15 @@ class ImageSubscriber(Node):
                     cv2.rectangle(self.__cv_image, p1, p2, (0, 0, 255), 2)
 
                     # Draw text
-                    cv2.putText(self.__cv_image, class_name, (p1[0], p1[1] + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 0, 0), 1)
-
-                self.__cv_image = cv2.cvtColor(self.__cv_image, cv2.COLOR_RGB2BGR)
+                    cv2.putText(self.__cv_image, class_name, (p1[0], p1[1] + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 0), 1)
 
             # Loop through detection list
             for detection in detection_lst:
                 # Draw fonts on left corner of image
-                cv2.putText(self.__cv_image, detection[0], (10, 20 + 10 * detection_lst.index(detection)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+                cv2.putText(self.__cv_image, detection[0], (10, 20 + 10 * detection_lst.index(detection)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
 
-            
+            # Change color format
+            self.__cv_image = cv2.cvtColor(self.__cv_image, cv2.COLOR_RGB2BGR)
 
             msg = CompressedImage()
             msg.header = self.__cv_image_header
@@ -127,13 +130,10 @@ class ImageSubscriber(Node):
 
 
     def listener_callback(self, data):
-        # Convert to greyscale
-        cv_image = self.__cv_br.imgmsg_to_cv2(data)
-
         # Copy image to self.__cv_image
         with self.__cv_image_lock:
             self.__cv_image_header = data.header
-            self.__cv_image = cv_image.copy()
+            self.__cv_image = self.__cv_br.imgmsg_to_cv2(data, desired_encoding='rgb8').copy()
 
 def main(args=None):
     rclpy.init(args=args)
